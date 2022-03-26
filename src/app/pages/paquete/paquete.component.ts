b0c59352d0cf872a995d90f7bae1e558700ee1f6
Emir <emirtapiamendez@gmail.com>
2022-03-25 20:42:17 -0600

    zorro kit

--- src/app/pages/paquete/paquete.component.ts
+++ src/app/pages/paquete/paquete.component.ts
@@ -57,6 +57,23 @@ export class PaqueteComponent implements OnInit {
       duracion:'60 minutos',
       tipo:['Presencial'],
       tipoConsulta:1
+    },
+    {
+      nombre: 'Zorro kit',
+      precio: 600.00,
+      preciopaquete: 5800.00,
+      dirigido: 'Dirigido para mujeres que planean embarazarse o están embarazadas.',
+      descripcion: ['•	Consulta nutricia quincenal, para conocer objetivos, evaluación del estado nutricio actual o del trimestre que te encuentres.',
+        '•	Recomendaciones alimentarias afines a tu trimestre de gestación o para el inicio de la concepción.',
+        '•	Antropometría (monitoreo de ganancia de peso adecuada para cada trimestre del embarazo).',
+        '•	Orientación en la selección del mejor multivitamínico.',
+        '•	Lista del súper y lista de equivalentes. ',
+        '•	Plan de alimentación individualizado por equivalentes y 2 opciones de menú personalizados que incluyen ideas de hidratación, metas y recomendaciones alimentarias afines a tus objetivos. ',
+        '•	Resolución de dudas y apoyo vía WhatsApp.',
+        '•	Sesiones educativas con temas de alimentación.'],
+      duracion: '60 minutos',
+      tipo: ['CDMX - CHPO'],
+      tipoConsulta: 3
     }]
   }
 
