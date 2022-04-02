a9abafdbc47760bf554548bc84061a438bbf8cd5
Emir <emirtapiamendez@gmail.com>
2022-04-02 14:21:25 -0600

    Actualizacion paquetes y faq

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Faq } from './../../models/faq.model';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
+  public faqs: Faq[];
+  constructor() {
+    this.faqs=[
+    {pregunta:'Como agendo una cita?',respuesta:'Envianos un correo con el paquete de tu interes asi como tus datos personales para poder agendar.'},
+    {pregunta:'Como agendo una cita?',respuesta:'Envianos un correo con el paquete de tu interes asi como tus datos personales para poder agendar.'},
+    {pregunta:'Como agendo una cita?',respuesta:'Envianos un correo con el paquete de tu interes asi como tus datos personales para poder agendar.'},
+    {pregunta:'Como agendo una cita?',respuesta:'Envianos un correo con el paquete de tu interes asi como tus datos personales para poder agendar.'}
+  ]
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
+    this.faqs
   }
 
 }
